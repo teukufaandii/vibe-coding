3253ff087103c831d8228e7954a9f26f753ac50c
T.Fandi Ahmad Fadillah <t.fandiahmadfadillah@172-10-66-16.lightspeed.jcsnms.sbcglobal.net>
2026-04-24 11:32:22 +0700

    feat: project setup with nestjs, prisma, and postgresql (closes #1)

---
+++ src/app.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Get()
+  async getUsers() {
+    return this.prisma.user.findMany();
+  }
+}
