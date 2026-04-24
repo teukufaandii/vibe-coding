23ebdcd09096958c4843f40f59067c48ce9f11e4
T.Fandi Ahmad Fadillah <t.fandiahmadfadillah@172-10-66-16.lightspeed.jcsnms.sbcglobal.net>
2026-04-24 14:08:49 +0700

    feat: setup redis cache and config (closes #2)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import * as cache from 'cache-manager';
 import { PrismaService } from './prisma/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly prisma: PrismaService) {}
-
-  @Get()
-  async getUsers() {
-    return this.prisma.user.findMany();
-  }
+  constructor(
+    private readonly prisma: PrismaService,
+    @Inject(CACHE_MANAGER) private cacheManager: cache.Cache,
+  ) {}
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { redisStore } from 'cache-manager-redis-yet';
 import { AppController } from './app.controller';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        store: await redisStore({
+          socket: {
+            host: configService.get<string>('REDIS_HOST'),
+            port: parseInt(configService.get<string>('REDIS_PORT')!),
+          },
+        }),
+      }),
+      inject: [ConfigService],
+    }),
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
